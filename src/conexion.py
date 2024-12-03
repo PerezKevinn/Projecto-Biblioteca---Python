d146ab78d0d56cbad2123eab612ef4b2dd3fa7c6
Kevin Perez <kp5505114@gmail.com>
2024-12-03 10:33:31 -0500

    #

--- src/conexion.py
+++ src/conexion.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import menu
 from tkinter import messagebox as mb
 from mysql.connector import Error
 
@@ -29,3 +28,84 @@ def login(username, password):
         mb.showerror(title='Error de conexion', message='Error al conectar con la base de datos')
     cursor.close()
     connection.close()
+
+def cargar_datos(tree):
+    # Conectar a la base de datos MySQL en WAMPServer
+    try:
+        connection = connect_db()
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            # Consulta SQL para obtener los datos de la tabla
+            query = "SELECT id_libro, titulo, autor, editorial, categoria, estado FROM libros"
+            cursor.execute(query)
+
+            # Recuperar todos los resultados
+            rows = cursor.fetchall()
+
+            # Limpiar el Treeview antes de insertar nuevos datos
+            for row in tree.get_children():
+                tree.delete(row)
+
+            # Insertar los resultados en el Treeview
+            for row in rows:
+                tree.insert("", "end", values=row)
+
+    except Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+    
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+def insertar_dato(codigo, titulo, autor, editorial, categoria, estado):
+    connection = connect_db()
+    cursor = connection.cursor()
+    
+    query = """
+    INSERT INTO libros (id_libro, titulo, autor, editorial, categoria, estado)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    values = (codigo, titulo, autor, editorial, categoria, estado)
+    cursor.execute(query, values)
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def eliminar_dato(isbn):
+    connection = connect_db()
+    cursor = connection.cursor()
+    
+    query = "DELETE FROM libros WHERE id_libro = %s"
+    cursor.execute(query, (isbn,))
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def obtener_datos():
+        # Conectar a la base de datos SQLite
+        connection = connect_db()
+        cursor = connection.cursor()
+        
+        query = "SELECT id_libro, titulo, autor, editorial, categoria, estado FROM libros"
+        
+        cursor.execute(query)
+        
+        resultados = cursor.fetchall()
+        connection.close()
+        return resultados
+
+def obtener_libros():
+    connection = connect_db()
+    cursor = connection.cursor()
+    
+    cursor.execute("SELECT titulo FROM libros")
+    
+    libros = cursor.fetchall()
+    
+    connection.close()
+    return [titulo[0] for titulo in libros]
--- src/menu.py
+++ src/menu.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 import customtkinter as ctk
 import tooltip
-import login
+import conexion
 from tkinter import messagebox as mb
+from tkinter import ttk
 from PIL import Image
 
 class App():
@@ -101,26 +102,50 @@ class App():
         self.TxtConsulta = ctk.CTkEntry(self.FrButtons, width=400, fg_color='#FFFFFF', font=('Roboto', 18), text_color='#000000', placeholder_text='ISBN / ID / NOMBRE', placeholder_text_color='#a1a0a0')
         self.TxtConsulta.place(relx=0.26, rely=0.40)
         
+        # Configurar el evento de cambio de texto en TxtConsulta
+        self.TxtConsulta.bind("<KeyRelease>", self.consultar_dinamicamente)
+        
         # Frame Grilla
         self.FrGrilla = ctk.CTkFrame(self.FrInventory, width=800, height=500, corner_radius=30, fg_color='#005066')
         self.FrGrilla.place(relx=0.05, rely=0.20)
-              
+        
+        # Treeview
+        self.Tree = ttk.Treeview(self.FrGrilla, columns=('ISBN', 'Titulo', 'Autor', 'Editorial', 'Categoria', 'Estado', 'Fecha_Ingreso'))
+        
+        # Configurar los headings
+        self.Tree.heading("#0", text="")
+        self.Tree.heading("ISBN", text="ISBN")
+        self.Tree.heading("Titulo", text="Título")
+        self.Tree.heading("Autor", text="Autor")
+        self.Tree.heading("Editorial", text="Editorial")
+        self.Tree.heading("Categoria", text="Categoría")
+        self.Tree.heading("Estado", text="Estado")
+        self.Tree.heading("Fecha_Ingreso", text="Fecha Ingreso")
+        
+        # Configurar la columna
+        self.Tree.column("#0", width=0)
+        self.Tree.column("ISBN", width=100)
+        self.Tree.column("Titulo", width=140)
+        self.Tree.column("Autor", width=120)
+        self.Tree.column("Editorial", width=120)
+        self.Tree.column("Categoria", width=80)
+        self.Tree.column("Estado", width=60)
+        self.Tree.column("Fecha_Ingreso", width=70)
+        
+        # Posicionar el Tree
+        self.Tree.place(relx=0.06, rely=0.20)
+        
+        # Llenar datos
+        conexion.cargar_datos(self.Tree)
+        
         # CRUD
-        self.BtnNuevo = ctk.CTkButton(self.FrGrilla, width=60, height=50, image=self.Nuevo, text='', fg_color='#005066', border_width=1, border_color='#FFFFFF')
-        self.BtnNuevo.place(relx=0.88, rely=0.10)
+        self.BtnNuevo = ctk.CTkButton(self.FrGrilla, width=60, height=50, image=self.Nuevo, text='', fg_color='#005066', border_width=1, border_color='#FFFFFF', command=self.open_add_record_window)
+        self.BtnNuevo.place(relx=0.13, rely=0.06)
         tooltip.Hovertip(self.BtnNuevo, text='Agregar nuevo registro', hover_delay=100)
         
-        self.BtnEditar = ctk.CTkButton(self.FrGrilla, width=60, height=50, image=self.Editar, text='', fg_color='#005066', border_width=1, border_color='#FFFFFF')
-        self.BtnEditar.place(relx=0.88, rely=0.22)
-        tooltip.Hovertip(self.BtnEditar, text='Editar registro seleccionado', hover_delay=100)
-        
-        self.BtnEliminar = ctk.CTkButton(self.FrGrilla, width=60, height=50, image=self.Eliminar, text='', fg_color='#005066', border_width=1, border_color='#FFFFFF')
-        self.BtnEliminar.place(relx=0.88, rely=0.34)
+        self.BtnEliminar = ctk.CTkButton(self.FrGrilla, width=60, height=50, image=self.Eliminar, text='', fg_color='#005066', border_width=1, border_color='#FFFFFF', command=self.eliminar_dato)
+        self.BtnEliminar.place(relx=0.33, rely=0.06)
         tooltip.Hovertip(self.BtnEliminar, text='Eliminar registro seleccionado', hover_delay=100)
-        
-        self.BtnBuscar = ctk.CTkButton(self.FrGrilla, width=60, height=50, image=self.Buscar, text='', fg_color='#005066', border_width=1, border_color='#FFFFFF')
-        self.BtnBuscar.place(relx=0.88, rely=0.46)
-        tooltip.Hovertip(self.BtnBuscar, text='Buscar registro', hover_delay=100)
     def Loans_Form(self): 
         
         # Frame Prestamos
@@ -185,7 +210,6 @@ class App():
         # Entry Codigo
         self.TxtCodigo = ctk.CTkEntry(self.FrButtons, text_color='#000000', fg_color='#FFFFFF', width=290, height=4, font=('Roboto', 18))
         self.TxtCodigo.place(relx=0.40, rely=0.40)
-    def Information_Form(self):
         
     # Frames
     def Create_Search(self):
@@ -230,31 +254,32 @@ class App():
         self.TxtNombre.place(relx=0.40, rely=0.20)
         
         # Label Telefono
-        self.LblTel = ctk.CTkLabel(self.FrDatos, width=280, fg_color='#005066', text='Telefono', text_color='#FFFFFF', font=('Roboto', 18), anchor='w')
-        self.LblTel.place(relx=0.20, rely=0.30)
+        self.LblFechaPrestamo = ctk.CTkLabel(self.FrDatos, width=280, fg_color='#005066', text='Fecha prestamo', text_color='#FFFFFF', font=('Roboto', 18), anchor='w')
+        self.LblFechaPrestamo.place(relx=0.20, rely=0.30)
         
         # Entry Telefono
-        self.TxtTel = ctk.CTkEntry(self.FrDatos, width=280, fg_color='#FFFFFF', text_color='#000000', font=('Roboto', 18))
-        self.TxtTel.place(relx=0.40, rely=0.30)
+        self.TxtFechaPrestamo = ctk.CTkEntry(self.FrDatos, width=280, fg_color='#FFFFFF', text_color='#000000', font=('Roboto', 18))
+        self.TxtFechaPrestamo.place(relx=0.40, rely=0.30)
 
         # Label Ficha
-        self.LblFicha = ctk.CTkLabel(self.FrDatos, width=280, fg_color='#005066', font=('Roboto', 18), text_color='#FFFFFF', text='Ficha', anchor='w')
-        self.LblFicha.place(relx=0.20, rely=0.40)
+        self.LblFechaDevolucion = ctk.CTkLabel(self.FrDatos, width=280, fg_color='#005066', font=('Roboto', 18), text_color='#FFFFFF', text='Fecha Devolucion', anchor='w')
+        self.LblFechaDevolucion.place(relx=0.20, rely=0.40)
         
         # Entry Ficha
-        self.TxtFicha = ctk.CTkEntry(self.FrDatos, width=280, fg_color='#FFFFFF', font=('Roboto', 18), text_color='#000000')
-        self.TxtFicha.place(relx=0.40, rely=0.40)
+        self.TxtFechaDevolucion = ctk.CTkEntry(self.FrDatos, width=280, fg_color='#FFFFFF', font=('Roboto', 18), text_color='#000000')
+        self.TxtFechaDevolucion.place(relx=0.40, rely=0.40)
         
         # Separador
         self.Separador = ctk.CTkLabel(self.FrDatos, width=670, fg_color='#FFFFFF', text='', font=('Roboto', 2), height=2)
         self.Separador.place(relx=0.08, rely=0.52)
         
         # Combobox Items
-        self.CbItems = ctk.CTkComboBox(self.FrDatos, fg_color='#FFFFFF', text_color='#000000', font=('Roboto', 18),  width=320, button_color='#f09641')
+        libros = conexion.obtener_libros()
+        self.CbItems = ctk.CTkComboBox(self.FrDatos, fg_color='#FFFFFF', text_color='#000000', font=('Roboto', 18),  width=320, button_color='#f09641', values=libros, state='readonly')
         self.CbItems.place(relx=0.29, rely=0.62)
         
         # Btn Guardar
-        self.BtnGuardar = ctk.CTkButton(self.FrDatos, fg_color='#005066', text='Guardar', font=('Roboto', 18, 'bold'), width=150, height=80, border_color='#FFFFFF', border_width=1, compound='left', image=self.Guardar, hover=True, hover_color='#002029')
+        self.BtnGuardar = ctk.CTkButton(self.FrDatos, fg_color='#005066', text='Guardar', font=('Roboto', 18, 'bold'), width=150, height=80, border_color='#FFFFFF', border_width=1, compound='left', image=self.Guardar, hover=True, hover_color='#002029', command=self.Guardar_Prestamo)
         self.BtnGuardar.place(relx=0.25, rely=0.72)
         
         # Btn Cancelar
@@ -264,7 +289,148 @@ class App():
     # Commands
     def Home(self):
         self.Loans_Form()
+    def open_add_record_window(self):
+        # Crear una nueva ventana encima de la principal
+        add_window = tk.Toplevel(self.FrInventory)
+        add_window.title("Agregar Nuevo Registro")
+        add_window.config(bg="#002029")
+    
+        # Obtener las dimensiones de la pantalla y la ventana principal
+        screen_width = add_window.winfo_screenwidth()
+        screen_height = add_window.winfo_screenheight()
+        window_width = 400
+        window_height = 400
+
+        # Calcular la posición centrada en la pantalla
+        position_top = int((screen_height / 2) - (window_height / 2))
+        position_left = int((screen_width / 2) - (window_width / 2))
+
+        # Establecer la geometría para centrar la ventana
+        add_window.geometry(f"{window_width}x{window_height}+{position_left}+{position_top}")
+
+        # Desactivar la opción de redimensionar
+        add_window.resizable(False, False)
+    
+        # Etiquetas y campos de entrada para los datos
+        label_codigo = tk.Label(add_window, text="Código", font=("Roboto", 12), bg="#002029", fg="#FFFFFF")
+        label_codigo.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.TxtCodigoAdd = tk.Entry(add_window, font=("Roboto", 12))
+        self.TxtCodigoAdd.grid(row=0, column=1, padx=10, pady=10)
+
+        label_titulo = tk.Label(add_window, text="Titulo", font=("Roboto", 12), bg="#002029", fg="#FFFFFF")
+        label_titulo.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        self.TxtTituloAdd = tk.Entry(add_window, font=("Roboto", 12))
+        self.TxtTituloAdd.grid(row=1, column=1, padx=10, pady=10)
+
+        label_autor = tk.Label(add_window, text="Autor", font=("Roboto", 12), bg="#002029", fg="#FFFFFF")
+        label_autor.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+        self.TxtAutorAdd = tk.Entry(add_window, font=("Roboto", 12))
+        self.TxtAutorAdd.grid(row=2, column=1, padx=10, pady=10)
+
+        label_Editorial = tk.Label(add_window, text="Editorial", font=("Roboto", 12), bg="#002029", fg="#FFFFFF")
+        label_Editorial.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+        self.TxtEditorialAdd = tk.Entry(add_window, font=("Roboto", 12))
+        self.TxtEditorialAdd.grid(row=3, column=1, padx=10, pady=10)
+
+        label_categoria = tk.Label(add_window, text="Categoría", font=("Roboto", 12), bg="#002029", fg="#FFFFFF")
+        label_categoria.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+        self.CbItemsAdd = ttk.Combobox(add_window, values=["Distopía", "Clásico", "Misterio", "Romance"], font=("Roboto", 12))
+        self.CbItemsAdd.grid(row=4, column=1, padx=10, pady=10)
+    
+        label_estado = tk.Label(add_window, text="Estado", font=("Roboto", 12), bg="#002029", fg="#FFFFFF")
+        label_estado.grid(row=5, column=0, padx=10, pady=10, sticky="w")
+        self.CbEstadoAdd = ttk.Combobox(add_window, values=["Disponible", "No disponible"], font=("Roboto", 12))
+        self.CbEstadoAdd.grid(row=5, column=1, padx=10, pady=10)
+
+        # Botón para guardar el nuevo registro
+        save_button = tk.Button(add_window, text="Guardar", font=("Roboto", 12), bg="#005066", fg="#FFFFFF", command=self.save_new_record)
+        save_button.grid(row=6, column=0, columnspan=2, pady=20)
+    
+        # Botón para cerrar la ventana emergente
+        cancel_button = tk.Button(add_window, text="Cancelar", font=("Roboto", 12), bg="#D32F2F", fg="#FFFFFF", command=add_window.destroy)
+        cancel_button.grid(row=7, column=0, columnspan=2, pady=10)
+
+        # Hacer que la ventana se cierre automáticamente cuando se presione el botón "Guardar"
+        add_window.transient(self.FrInventory)
+        add_window.grab_set()
+    def save_new_record(self):
+        # Obtener los datos de los campos de entrada
+        codigo = self.TxtCodigoAdd.get()
+        titulo = self.TxtTituloAdd.get()
+        autor = self.TxtAutorAdd.get()
+        editorial = self.TxtEditorialAdd.get()
+        categoria = self.CbItemsAdd.get()
+        estado = self.CbEstadoAdd.get()
+    
+        # Verificar si todos los campos están llenos
+        if not codigo or not titulo or not autor or not editorial or not categoria or not estado:
+            print("Por favor complete todos los campos")
+            return
+    
+        # Insertar los datos en la base de datos
+        conexion.insertar_dato(codigo, titulo, autor, editorial, categoria, estado)
+    
+        # Cerrar la ventana de agregar registro
+        self.TxtCodigoAdd.delete(0, tk.END)
+        self.TxtTituloAdd.delete(0, tk.END)
+        self.TxtAutorAdd.delete(0, tk.END)
+        self.TxtEditorialAdd.delete(0, tk.END)
+        self.CbItemsAdd.set('')
+        self.CbEstadoAdd.set('')
+    
+        # Recargar los datos en el Treeview de la ventana principal
+        self.Inventory_Panel()
+    def eliminar_dato(self):
+        # Obtener el ID del item seleccionado
+        selected_item = self.Tree.selection()
+
+        if not selected_item:  # Verificar si se ha seleccionado algún item
+            mb.showwarning("Selección vacía", "Por favor, selecciona un registro para eliminar.")
+            return
+
+        # Obtener los valores del item seleccionado
+        item_values = self.Tree.item(selected_item)['values']
+    
+        # Suponiendo que 'ISBN' es el primer valor del item, lo usamos para eliminar el registro
+        isbn = item_values[0]
+
+        # Confirmar con el usuario si desea eliminar el registro
+        confirm = mb.askyesno("Confirmar eliminación", f"¿Estás seguro de que deseas eliminar el registro con ISBN: {isbn}?")
+
+        if confirm:
+            # Llamar a la función para eliminar el registro de la base de datos
+            try:
+             # Suponiendo que la función `conexion.eliminar_dato(isbn)` elimina el registro de la base de datos
+                conexion.eliminar_dato(isbn)
             
+                # Eliminar el item del Treeview
+                self.Tree.delete(selected_item)
+
+                # Mostrar mensaje de éxito
+                mb.showinfo("Eliminación exitosa", f"El registro con ISBN {isbn} ha sido eliminado.")
+        
+            except Exception as e:
+                # Si ocurre algún error, mostrar un mensaje
+                mb.showerror("Error", f"Hubo un error al intentar eliminar el registro: {e}")
+        # Obtener los valores de los campos
+    def consultar_dinamicamente(self, event):
+        # Obtener el texto de la caja de texto de consulta
+        search_text = self.TxtConsulta.get().lower()
+    
+        for item in self.Tree.get_children():
+            self.Tree.delete(item)
+    
+        all_data = conexion.obtener_datos()
+    
+        filtered_data = []
+        for record in all_data:
+            if any(search_text in str(field).lower() for field in record):
+                filtered_data.append(record)
+    
+        for record in filtered_data:
+            self.Tree.insert("", "end", values=record)
+        mb.showwarning(title="Resultado", message=mensaje).show()
+        
     # Indicadores
     def Hide_Indicators(self):
         self.IndInventario.configure(bg='#005066')
@@ -318,13 +484,6 @@ class App():
         
         self.BtnSanciones = ctk.CTkButton(self.FrNav, fg_color='#005066', text='Sanciones', image=self.Sanciones, width=193, height=10, compound='left', corner_radius=0, font=('Roboto', 18, 'bold'), anchor='w', border_spacing=10, hover=False, command=lambda:self.Indicators(self.IndSanciones, self.Sanctions_Form))
         self.BtnSanciones.place(relx=0, rely=0.67)
-        
-        # Btn Information
-        self.IndInformation = tk.Label(self.FrNav, background='#005066', width=5, height=3, text='')
-        self.IndInformation.place(relx=0.97, rely=0.67)
-        
-        self.BtnInformation = ctk.CTkButton(self.FrNav, fg_color='#005066', text='Informes', image=self.Informes, width=193, height=10, compound='left', corner_radius=0, font=('Roboto', 18, 'bold'), anchor='w', border_spacing=10, hover=False, command=lambda:self.Indicators(self.IndInformation, self.Information_Form))
-        self.BtnInformation.place(relx=0, rely=0.76)
         
         # Btn Salir
         self.BtnSalir = ctk.CTkButton(self.FrNav, fg_color='#005066', text='Cerrar Sesion', image=self.Salir, width=250, height=10, compound='left', corner_radius=0, font=('Roboto', 18, 'bold'), anchor='w', border_spacing=10, hover=False)
